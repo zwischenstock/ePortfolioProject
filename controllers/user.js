edacc7ddb4603cbcea52828648a656dc84d9cefc
zwischenstock <67775106+zwischenstock@users.noreply.github.com>
2021-06-21 05:05:52 +0900

    Added bruteforce prevention for login

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,10 @@
 var user_model = require('../models/user')
 var { Roles } = require('../user')
 
+const THIRTY_SECONDS = 1000 * 30
+const NINETY_SECONDS = THIRTY_SECONDS * 3
+const THIRTY_MINUTES = (THIRTY_SECONDS * 2) * 30
+
 exports.setup = function(app, util) {
 	app.post('/login', function(req, res) {
 		// Something fishy is going on if they try to log in while already logged in.
@@ -14,10 +18,33 @@ exports.setup = function(app, util) {
 		if (username == null || password == null || typeof username != 'string' || typeof password != 'string')
 			return util.render(req, res, 'index', {alert: "Invalid username or password"})
 
-		user_model.tryLogin(username, password, function(success, user, err) {
-			if (!success)
-				return util.render(req, res, 'index', {alert: "Invalid username or password"})
+		var now = new Date().getTime()
+		if (req.session.loginSpam && req.session.loginSpam > now) {
+			var diff = new Date(req.session.loginSpam - now)
+			var tstring = diff.getMinutes() + " minute" + (diff.getMinutes() == 1 ? "" : "s") + " and " +
+				diff.getSeconds() + " second" + (diff.getSeconds() == 1 ? "" : "s")
 
+			return util.render(req, res, 'index', {alert: "You cannot attempt again for another " + tstring})
+		}
+
+		user_model.tryLogin(username, password, function(success, user, err) {
+			if (!success) {
+				// Every time the user fails a login, 30 seconds are added to their first recorded fail time.
+				// If that time is 90 seconds below the current time, it's reset to the current time.
+				// If that time adds up to 90 seconds over the current time, they are stopped from logging in for 30 minutes.
+				if (!req.session.loginTimeout || (now - req.session.loginTimeout) > NINETY_SECONDS)  {
+					req.session.loginTimeout = now
+				} else if (req.session.loginTimeout > (now + NINETY_SECONDS)) {
+					req.session.loginSpam = now + THIRTY_MINUTES
+				} else {
+					req.session.loginTimeout = req.session.loginTimeout + THIRTY_SECONDS
+				}
+
+				return util.render(req, res, 'index', {alert: "Invalid username or password"})
+			}
+
+			req.session.loginTimeout = null
+			req.session.loginSpam = null
 			req.session.user = user.toObject()
 
 			res.redirect('/zoo')
@@ -56,8 +83,9 @@ exports.setup = function(app, util) {
 	})
 
 	app.post('/newpassword', function(req, res) {
-		if (!util.isUser(req))
+		if (!util.isUser(req)) {
 			return util.render(req, res, 'profile', {alert: 'You must be signed in to do this'})
+		}
 
 		var old_pass = req.body.oldPassword
 		var new_pass = req.body.newPassword
